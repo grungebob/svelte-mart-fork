2f320581149a1554f0512bfda74315ab8b78610f
grungebob <bobgrunge@gmail.com>
2026-05-29 00:14:09 -0400

    totals for cart, and using a derived to control an effect

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -8,16 +8,48 @@
 	let cartOpen = $state(false);
 	let cartProducts = $state<CartProduct[]>([]);
 	// let cartQuantityTotal = $derived(cartProducts.reduce((acc, cartProduct) => acc + cartProduct.quantity, 0));
-	const cartQuantity = $derived.by(()=> {
-		let total = 0;
+	// const cartQuantity = $derived.by(()=> {
+	// 	let total = 0;
+	// 	for (const product of cartProducts) {
+	// 		total += product.quantity;
+	// 	}
+	// 	return total;
+	// });
+	const cartStats = $derived.by(()=> {
+		let totalQuantity = 0;
+		let totalPrice = 0;
 		for (const product of cartProducts) {
-			total += product.quantity;
+			totalQuantity += product.quantity;
+			totalPrice += product.quantity * product.product.price;
 		}
-		return total;
+		return {
+			totalQuantity,
+			totalPrice
+		};
 	});
 	const removeFromCart = (id: string) => {
 		cartProducts = cartProducts.filter((product) => product.id !== id);
 	};
+
+	// use a derived state to check for a change to the status of free shipping
+	// rather than an effect because it will run every time the price changes above 50
+
+	const qualifiesForFreeShipping = $derived(cartStats.totalPrice >= 50);
+
+	$effect(()=> {
+		// if(cartStats.totalPrice > 50) {
+		// 	alert('You have qualified for free shipping yo.');
+		// }
+		if(qualifiesForFreeShipping) {
+			alert('You have qualified for free shipping yo.');
+		}
+	})
+
+	$effect(()=> {
+		if(cartStats.totalQuantity === 0 && cartOpen) {
+			alert('Your cart is empty. Add items to your cart!!!!!');
+		}
+	})
 </script>
 
 <div class="flex items-center bg-gray-300 p-4">
@@ -25,7 +57,7 @@
 	<div class="relative ml-auto flex items-center">
 		<button onclick={() => (cartOpen = !cartOpen)} class="flex items-center rounded-full bg-sky-600 px-4 py-2 text-white hover:bg-sky-700">
 			<ShoppingCart class="mr-2 size-5" />
-			<span>Cart ({cartQuantity})</span>
+			<span>Cart ({cartStats.totalQuantity})</span>
 		</button>
 		{#if cartOpen}
 		<div class="absolute right-0 top-8 z-10 mt-2 w-80 rounded-lg bg-white shadow-xl">
@@ -34,12 +66,14 @@
 				<button onclick={() => (cartOpen = false)} class="absolute right-4 top-4 rounded-full p-1 hover:bg-gray-100">
 					<X class="size-4" />
 				</button>
+				{#if cartProducts.length > 0}	
 				{#each cartProducts as _, index}
 					<CartItem bind:cartProduct={cartProducts[index]} {removeFromCart} />
 				{/each}
 				<div class="mt-4 border-gray-200 pt-4">
-					<p class="text-lg font-semibold">Total: $39.98</p>
+					<p class="text-lg font-semibold">Total: ${cartStats.totalPrice.toFixed(2)}</p>
 				</div>
+				{/if}
 			</div>
 		</div>
 		{/if}
